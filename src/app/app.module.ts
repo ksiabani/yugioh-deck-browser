cd93ef5ef3f5bcbe1dc3907d958a4067616378ee
siampako <konstantinos.siampanis@arhs-dev-hellas.com>
2018-10-23 17:53:58 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { MatButtonModule, MatIconModule } from "@angular/material";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
