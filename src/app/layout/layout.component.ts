fbd574a59f77436286269dd4356a3298ee4984a4
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-10-27 09:52:03 +0300

    Add more card properties, close sidenav on click

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { Observable } from "rxjs";
 import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 import { map } from "rxjs/operators";
 import { Select } from "@ngxs/store";
 import { DeckState } from "../shared/store/deck.state";
+import { MatSidenav } from "@angular/material";
 
 @Component({
   selector: "app-layout",
@@ -11,16 +12,27 @@ import { DeckState } from "../shared/store/deck.state";
   styleUrls: ["./layout.component.scss"]
 })
 export class LayoutComponent implements OnInit {
-  isHome: boolean;
   isMobile: Observable<boolean> = this.breakpointObserver
-    .observe([Breakpoints.Handset, Breakpoints.TabletPortrait, Breakpoints.WebPortrait])
+    .observe([
+      Breakpoints.Handset,
+      Breakpoints.TabletPortrait,
+      Breakpoints.WebPortrait
+    ])
     .pipe(map(result => result.matches));
   @Select(DeckState.cardName)
   cardName: Observable<string>;
+  @ViewChild("sidenav")
+  sidenav: MatSidenav;
 
-  constructor(
-    private breakpointObserver: BreakpointObserver
-  ) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  closeSidenav() {
+    if (this.breakpointObserver.isMatched('(max-width: 959px)')) {
+      this.sidenav.close();
+    }
+  }
 }
